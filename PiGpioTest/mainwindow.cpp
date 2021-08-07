fa65c8c5e456a92fc15ffd0944b7ae142996667d
zhaoyu <860150543@qq.com>
2021-08-07 10:30:52 +0800

    树莓派4B GPIO测试

---
+++ PiGpioTest/mainwindow.cpp
@@ -0,0 +1,123 @@
+#include "mainwindow.h"
+#include <QGridLayout>
+#include <QDebug>
+#include <wiringPi.h>
+
+/*
+pi@raspberrypi:~ $ gpio pins
+ +-----+-----+---------+------+---+---Pi 4B--+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
+ |   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5v      |     |     |
+ |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
+ |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 1 | IN   | TxD     | 15  | 14  |
+ |     |     |      0v |      |   |  9 || 10 | 1 | IN   | RxD     | 16  | 15  |
+ |  17 |   0 | GPIO. 0 |  OUT | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |
+ |  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
+ |  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
+ |     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
+ |  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
+ |   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
+ |  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
+ |     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
+ |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
+ |   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |
+ |   6 |  22 | GPIO.22 |   IN | 1 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
+ |  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
+ |  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
+ |  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
+ |     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+---Pi 4B--+---+------+---------+-----+-----+
+ */
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    QWidget * widget = new QWidget(this);
+    QGridLayout *mainLayout = new QGridLayout(widget);
+    widget->setLayout(mainLayout);
+
+    //1. 初始化checkBox
+    QString pinNames("3.3v,GPIO.8,GPIO.9,GPIO.7,0v,GPIO.0,GPIO.2,GPIO.3,3.3v,GPIO.12,GPIO.13,GPIO.14,0v,GPIO.30,GPIO.21,\
+            GPIO.22,GPIO.23,GPIO.24,GPIO.25,0v,5v,5v,0v,GPIO.15,GPIO.16,GPIO.1,0v,GPIO.4,GPIO.5,0v,GPIO.6,GPIO.10,GPIO.11,\
+            GPIO.31,0v,GPIO.26,0v,GPIO.27,GPIO.28,GPIO.29");
+    QStringList pinNamesList;
+    pinNamesList = pinNames.simplified().split(",");
+
+    for(int i=0; i<40; i++)
+    {
+        checkBox[i].setText(pinNamesList[i]);
+        if(checkBox[i].text().contains("0v"))
+        {
+            checkBox[i].setCheckState(Qt::Unchecked);
+            checkBox[i].setCheckable(false);
+            checkBox[i].setStyleSheet(QString(" QCheckBox::indicator:unchecked {\
+                            image: url(:/img/img/off.png);\
+                            }\
+                            QCheckBox::indicator:checked {\
+                            image: url(:/img/img/off.png);\
+                            }"));
+        }
+        else if(checkBox[i].text()=="3.3v" || checkBox[i].text()=="5v")
+        {
+            checkBox[i].setCheckState(Qt::Checked);
+            checkBox[i].setCheckable(false);
+            checkBox[i].setStyleSheet(QString(" QCheckBox::indicator:unchecked {\
+                            image: url(:/img/img/on.png);\
+                            }\
+                            QCheckBox::indicator:checked {\
+                            image: url(:/img/img/on.png);\
+                            }"));
+        }
+        else
+        {
+            connect(&checkBox[i], &QCheckBox::clicked, this, &MainWindow::CheckBoxClicked);
+            checkBox[i].setStyleSheet(QString(" QCheckBox::indicator:unchecked {\
+                            image: url(:/img/img/off.png);\
+                            }\
+                            QCheckBox::indicator:checked {\
+                            image: url(:/img/img/on.png);\
+                            }"));
+        }
+
+    }
+    //2. 放入Layout中
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<20; j++)
+            mainLayout->addWidget(&checkBox[j+(i*20)], i, j);
+    }
+
+    this->setCentralWidget(widget);
+    //3. 初始化GPIO
+    wiringPiSetup();
+    for(int i=0; i<40; i++)
+    {
+        if(pinNamesList[i].contains("GPIO"))
+        {
+            pinMode(pinNamesList[i].split(".").at(1).toInt(), OUTPUT);
+        }
+    }
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::CheckBoxClicked(bool checked)
+{
+    QCheckBox * checkBox = qobject_cast<QCheckBox *>(sender());
+    if(checked)
+    {
+        digitalWrite(checkBox->text().split(".").at(1).toInt(), 1);
+    }
+    else
+    {
+        digitalWrite(checkBox->text().split(".").at(1).toInt(), 0);
+    }
+}
+
